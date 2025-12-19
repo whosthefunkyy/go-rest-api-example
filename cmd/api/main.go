9b85543f74ab3a09a90972745fcf3d1f35346d67
Artem Melnychuk <127916553+whosthefunkyy@users.noreply.github.com>
2025-12-18 21:32:17 -0600

    Update main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,24 +6,27 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/whosthefunkyy/go-rest-api-example/db" 
 )
 
 func main() {
+	
+	db.ConnectGorm()
+	db.AutoMigrate()
+
 	r := mux.NewRouter()
 
-	// Наш главный эндпоинт для проверки здоровья
 	r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintf(w, "OK")
 	}).Methods("GET")
 
-	// Простой тестовый маршрут
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "API is running without DB for testing")
+		
+		fmt.Fprintf(w, "API is running WITH Database!")
 	}).Methods("GET")
 
 	log.Println("Server started at :5000")
-	// Важно: порт 8080 обязателен для EB Go платформы по умолчанию
 	if err := http.ListenAndServe(":5000", r); err != nil {
 		log.Fatal(err)
 	}
